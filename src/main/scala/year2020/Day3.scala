2c08eb36a66d3dc5a70e06a2ec1df3fa64677fb3
Sjors <sjors@protonmail.com>
2020-12-13 00:02:24 +0100

    Added solution for day 3

---
+++ src/main/scala/year2020/Day3.scala
@@ -0,0 +1,45 @@
+// package year2020
+
+import scala.io.Source
+
+object Day3 {
+
+  def defineGrid(input:String):Array[Array[Boolean]] = {
+    input.linesIterator.map { line =>
+      line.map(_ == '.').toArray
+    }.toArray
+  }
+
+  def countTrees(grid:Array[Array[Boolean]], right:Int, down:Int):Int = {
+    val l = for (i <- 0 until grid.length by down) yield {
+      val loc = (i * right/down) % grid(0).length
+      grid(i)(loc) == false
+    } 
+    l.count(_ == true)
+  }
+
+  def prodCountTrees(grid:Array[Array[Boolean]], steps: Array[(Int,Int)]):Long = {
+    val countedTrees = for ((r, d) <- steps) yield {
+      countTrees(grid, r, d)
+    }
+    countedTrees.map(_.toLong).product
+  }
+
+  def main(args:Array[String]): Unit = {
+    val input: String = Source.fromResource("year2020/day3.txt").mkString.trim
+
+    val steps = Array(
+      (1,1),
+      (3,1),
+      (5,1),
+      (7,1),
+      (1,2)
+    )
+
+    val grid: Array[Array[Boolean]] = defineGrid(input)
+    
+    println(countTrees(grid, 3, 1))
+    println(prodCountTrees(grid, steps))
+    
+  }
+}
