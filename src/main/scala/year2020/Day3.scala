894cc4d3aa19bf9764722ff4f04dfbab26548e29
Sjors <sjors@protonmail.ch>
2021-03-15 21:58:43 +0100

    Add solution for Day 4

--- src/main/scala/year2020/Day3.scala
+++ src/main/scala/year2020/Day3.scala
@@ -4,13 +4,9 @@ import scala.io.Source
 
 object Day3 {
 
-  def defineGrid(input: String): Array[Array[Boolean]] = {
-    input.linesIterator.map { line =>
-      line.map(_ == '.').toArray
-    }.toArray
-  }
+  type Grid[A] = Vector[Vector[A]]
 
-  def countTrees(grid: Array[Array[Boolean]], right: Int, down: Int): Int = {
+  def countTrees(grid: Grid[Boolean], right: Int, down: Int): Int = {
     val l = for (i <- 0 until grid.length by down) yield {
       val loc = (i * right / down) % grid(0).length
       grid(i)(loc) == false
@@ -18,25 +14,35 @@ object Day3 {
     l.count(_ == true)
   }
 
-  def prodCountTrees(grid: Array[Array[Boolean]], steps: Array[(Int, Int)]): Long = {
+  def prodCountTrees(grid: Grid[Boolean], steps: Seq[(Int, Int)]): Long = {
     val countedTrees = for ((r, d) <- steps) yield {
       countTrees(grid, r, d)
     }
     countedTrees.map(_.toLong).product
   }
 
+  lazy val input: String = Source.fromResource("year2020/day3.txt").mkString.trim
+
+  def defineGrid(input: String): Grid[Boolean] =
+    input.linesIterator
+      .map(_.toVector)
+      .toVector
+      .map(_.map({
+        case '#' => true
+        case '.' => false
+      }))
+
+  val multiplySlopes = Seq(
+    (1, 1),
+    (3, 1),
+    (5, 1),
+    (7, 1),
+    (1, 2)
+  )
+
   def main(args: Array[String]): Unit = {
-    val input: String = Source.fromResource("year2020/day3.txt").mkString.trim
 
-    val multiplySlopes = Array(
-      (1, 1),
-      (3, 1),
-      (5, 1),
-      (7, 1),
-      (1, 2)
-    )
-
-    val grid: Array[Array[Boolean]] = defineGrid(input)
+    val grid: Grid[Boolean] = defineGrid(input)
 
     println(countTrees(grid, 3, 1))
     println(prodCountTrees(grid, multiplySlopes))
